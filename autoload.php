31c772649037a6f8fce3665bc724f0e43a9ebfa3
lumacode <posicionatedigital@gmail.com>
2021-03-01 14:51:23 -0300

    Primer commit

---
+++ autoload.php
@@ -0,0 +1,28 @@
+<?php 
+
+/* 
+Cargamos los directorios en donde haremos la busqueda de clases
+@array  
+*/
+const DIRECTORY_SEARCH = ['controllers', 'middlewares'];
+
+function general_autoload($classname)
+{
+
+    foreach (DIRECTORY_SEARCH as $directory) 
+    {
+       /* 
+        Comprobamos que exista el directorio antes de incluir 
+       */
+        if(is_file("$directory/$classname.php"))
+        {
+            include "$directory/$classname.php";
+        }
+
+    }
+    
+}
+
+//Registramos la el auload de clases 
+spl_autoload_register('general_autoload');
+
---
+++ middlewares/ValidateJwt.php
@@ -0,0 +1,8 @@
+<?php 
+
+class ValidateJwt {
+
+
+    //
+
+}//endclass
